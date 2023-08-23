b3f8cc90b94be70d837c52cccf281cd8dfbda684
Alex Pripisnov <a.filtsov@gmail.com>
2023-08-23 22:46:48 +0300

    Files from folder 'src/main/java' have been updated

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,18 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(cat1.getName());
+
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(cat2.getName());
+
+        System.out.println("Is \'bean\' equal to \'bean2\'? " + (bean == bean2));
+        System.out.println("Is \'cat1\' equal to \'cat1\'? " + (cat1 == cat2));
     }
 }
